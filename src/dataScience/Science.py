674699c8eb16ede7430506c8105e9abe01e06e73
alejandro945 <alejo8677@gmail.com>
2021-07-31 21:52:26 -0500

    Some lsoc

--- src/dataScience/Science.py
+++ src/dataScience/Science.py
@@ -9,3 +9,18 @@ print(a.itemsize)
 # Reshape is when you change the number of [rows and columns] which gives a new view to an object.
 b = a.reshape(6,1)
 print(b)
+# Slicing
+print(a[1,2])
+#Looking for numbers in both rows? Here colon represents all the rows, including zero
+print(a[0:,1])
+#Linspace for print n numbers between a range; For example the follw expression will print 15 number in 10-30 range.
+c = np.linspace(10,30,15)
+print(c)
+# Sum an array in a blink
+print(a.sum())
+# Min and Max values
+print(c.max()) # => 30 -- Obviusly
+print(c.min()) # => 10 -- Obviusly
+# Data structures
+# Merge sort in-place, O(n log2 n)
+# Quicksort, O(n log n)
